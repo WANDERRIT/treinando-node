0d007a66b41ffa0da54d6cebeb1f63a58175858a
WANDERRIT <107007093+WANDERRIT@users.noreply.github.com>
2023-06-12 08:47:28 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,109 +1,59 @@
-//função = Ação/repetir um algoritimo sempre que necessário
+let listaArtistas = [{
+    nome: "Maria Rita",
+    estiloMusical: "MPB",
+    diasDisponiveis: ["sábado", "domingo"]
+    }, 
+    {nome: "Emicida",
+    estiloMusical: "Rap",
+    diasDisponiveis: ["sexta-feira", "sábado"]
+    },
+    {nome: "Liniker",
+    estiloMusical: "Soul",
+    diasDisponiveis: ["quinta-feira", "domingo"]
+    },
+    {nome: "Duda Beat",
+    estiloMusical: "Pop",
+    diasDisponiveis: ["quarta-feira", "sexta-feira"]
+    }];
 
-//criando a função
-//function <NomedaFunçao>
-// let nome = "wander"
-// function olaMundo(){
-//     console.log("Bom dia!! " + nome)
-// }
+    
 
-// //executando a função
-// olaMundo();
+    // let Emicida = listaArtistas
 
-
-// function olaMundo(nome ="Barbosa"){
-//     console.log("Bom dia!! " + nome)
-// }
-
-//executando a função
-// olaMundo("Wander");
-// olaMundo("Riti");
-// olaMundo();
+    // console.log(listaArtistas[1])
+    // console.log(Emicida.nome)
+    // console.log(Emicida)
 
 
 
-
-                                                            //EXERCÍCIOS DO 9 AO 1
-
-//9
-function converterEmHoras(minutos) {
-
-    console.log("Quantidade de minutos = " + minutos)
-
-    let horas = minutos / 60;
-
-    console.log("A conversão é = " + horas);
-
-}
-converterEmHoras(120);
-
-//exercício 8
-function tabuada(num = 1){
+function cadastrarArtista(nome, estiloMusical , diasDisponiveis){
+console.log(nome, estiloMusical, diasDisponiveis)
 }
 
+// cadastrarArtista("nome", "pop", ["sábado", "domingo!"])
 
-function tabuada(num){
-     
-    console.log("aqui é pra ser uma tabuada\n")
-    for(let i=1; i<=10; i++){
-        console.log(i * num)
+// let listaArtistas  = ["Halsey", "pop", ["sábado", "domingo"]];
+// console.log(listaArtistas);
+
+
+function  artistasDisponiveis(diaSemana){
+
+    
+    // for (var i = 0; i < listaArtistas.length; i++) {
+    //     var artista = listaArtistas[i];
+    //     if (artista.diasDisponiveis.includes(diaSemana)) {
+    //       console.log("Artista: " + artista.nome);
+    //       console.log("Estilo Musical: " + artista.estiloMusical);
+    //       console.log("----------------------");
+    //     }
+    //   }
+
+    for (let i = 0; i < listaArtistas.length; i++) {
+        if(listaArtistas[i].diasDisponiveis.includes(diaSemana)){
+            console.log(listaArtistas[i].nome)
+            console.log(listaArtistas[i].estiloMusical)
+
+        }
     }
-
 }
-tabuada(8);
-
-
-//7
-for (let i = 1; i <= 20; i=i +2) {
-    console.log(i);
-    }
-
-//6
-for (let i = 1; i <= 20; i++) {
-    console.log(i);
-    }
-
-//5
-function podeDirigir(num){
-    if(num >= 18){
-        console.log("pode dirigir");
-    } else {
-        console.log("Não pode dirigir")
-    }
-
-}
-
-podeDirigir(67);
-
-// 4
-function parametroNumericos(num, num2){
-    let resultado = num * num2;
-    console.log(resultado)
-}
-
-parametroNumericos(3, 3)
-
-// //3
-function saudacao(nome2 ="goku"){
-console.log("oi, eu sou o" + nome2)
-}
-saudacao();
-
-// //2 
-let numeroA = 2
-let numeroB = 2
-let resultado = numeroA * numeroB
-
-
-
-console.log(resultado)
-
-// //1
-let NomeCompleto = "Wander Riti Barbosa";
-
-console.log(NomeCompleto)
-
-
-
-
-
+    artistasDisponiveis("sexta-feira")
